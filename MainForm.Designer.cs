11030bbf779fc3f3b2e543841ede0799066e4460
diatanuchit <anuchit.aree.a9f@ap.denso.com>
2024-09-16 16:37:36 +0700

    master

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -45,12 +45,23 @@
             this.txtAddrIp = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtStartAddr = new System.Windows.Forms.TextBox();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtUpdate = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.txtTimeOut = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
             // lbStatus
             // 
             this.lbStatus.AutoSize = true;
-            this.lbStatus.Location = new System.Drawing.Point(185, 81);
+            this.lbStatus.Location = new System.Drawing.Point(270, 59);
             this.lbStatus.Name = "lbStatus";
             this.lbStatus.Size = new System.Drawing.Size(38, 13);
             this.lbStatus.TabIndex = 13;
@@ -59,7 +70,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(112, 347);
+            this.label9.Location = new System.Drawing.Point(154, 434);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(35, 13);
             this.label9.TabIndex = 4;
@@ -68,7 +79,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(112, 321);
+            this.label8.Location = new System.Drawing.Point(154, 408);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(35, 13);
             this.label8.TabIndex = 5;
@@ -77,7 +88,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(112, 294);
+            this.label7.Location = new System.Drawing.Point(154, 381);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(35, 13);
             this.label7.TabIndex = 6;
@@ -86,7 +97,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(112, 259);
+            this.label6.Location = new System.Drawing.Point(154, 346);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(35, 13);
             this.label6.TabIndex = 7;
@@ -95,7 +106,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(112, 228);
+            this.label5.Location = new System.Drawing.Point(154, 315);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 13);
             this.label5.TabIndex = 8;
@@ -104,7 +115,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(112, 200);
+            this.label4.Location = new System.Drawing.Point(154, 287);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 9;
@@ -113,7 +124,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(112, 177);
+            this.label3.Location = new System.Drawing.Point(154, 264);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 10;
@@ -122,7 +133,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(112, 144);
+            this.label2.Location = new System.Drawing.Point(154, 231);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 11;
@@ -131,7 +142,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(112, 110);
+            this.label1.Location = new System.Drawing.Point(154, 197);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 12;
@@ -139,7 +150,7 @@
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(188, 39);
+            this.btnConnect.Location = new System.Drawing.Point(260, 17);
             this.btnConnect.Name = "btnConnect";
             this.btnConnect.Size = new System.Drawing.Size(97, 39);
             this.btnConnect.TabIndex = 3;
@@ -149,7 +160,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(52, 408);
+            this.textBox1.Location = new System.Drawing.Point(94, 495);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(485, 20);
             this.textBox1.TabIndex = 14;
@@ -163,14 +174,14 @@
             "02 (0x02) Read Discrete Inputs",
             "03 (0x03) Read Holding Registers",
             "04 (0x04) Read Input Registers"});
-            this.cmbRegType.Location = new System.Drawing.Point(366, 39);
+            this.cmbRegType.Location = new System.Drawing.Point(19, 83);
             this.cmbRegType.Name = "cmbRegType";
             this.cmbRegType.Size = new System.Drawing.Size(219, 21);
             this.cmbRegType.TabIndex = 15;
             // 
             // txtPort
             // 
-            this.txtPort.Location = new System.Drawing.Point(415, 177);
+            this.txtPort.Location = new System.Drawing.Point(177, 27);
             this.txtPort.Name = "txtPort";
             this.txtPort.Size = new System.Drawing.Size(77, 20);
             this.txtPort.TabIndex = 16;
@@ -178,7 +189,7 @@
             // 
             // txtAddrIp
             // 
-            this.txtAddrIp.Location = new System.Drawing.Point(254, 177);
+            this.txtAddrIp.Location = new System.Drawing.Point(16, 27);
             this.txtAddrIp.Name = "txtAddrIp";
             this.txtAddrIp.Size = new System.Drawing.Size(155, 20);
             this.txtAddrIp.TabIndex = 17;
@@ -187,7 +198,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(254, 158);
+            this.label10.Location = new System.Drawing.Point(16, 8);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(58, 13);
             this.label10.TabIndex = 18;
@@ -196,23 +207,118 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(425, 161);
+            this.label11.Location = new System.Drawing.Point(187, 11);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(26, 13);
             this.label11.TabIndex = 18;
             this.label11.Text = "Port";
             // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(25, 67);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(73, 13);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "Register Type";
+            // 
+            // txtStartAddr
+            // 
+            this.txtStartAddr.Location = new System.Drawing.Point(257, 110);
+            this.txtStartAddr.Name = "txtStartAddr";
+            this.txtStartAddr.Size = new System.Drawing.Size(100, 20);
+            this.txtStartAddr.TabIndex = 19;
+            this.txtStartAddr.Text = "1";
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(363, 110);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(100, 20);
+            this.txtQty.TabIndex = 19;
+            this.txtQty.Text = "10";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(257, 94);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(69, 13);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "Start address";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(376, 91);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(31, 13);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "Q\'TY";
+            // 
+            // txtUpdate
+            // 
+            this.txtUpdate.Location = new System.Drawing.Point(469, 110);
+            this.txtUpdate.Name = "txtUpdate";
+            this.txtUpdate.Size = new System.Drawing.Size(100, 20);
+            this.txtUpdate.TabIndex = 19;
+            this.txtUpdate.Text = "1";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(477, 91);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(88, 13);
+            this.label15.TabIndex = 13;
+            this.label15.Text = "Auto-update(sec)";
+            // 
+            // txtTimeOut
+            // 
+            this.txtTimeOut.Location = new System.Drawing.Point(479, 36);
+            this.txtTimeOut.Name = "txtTimeOut";
+            this.txtTimeOut.Size = new System.Drawing.Size(100, 20);
+            this.txtTimeOut.TabIndex = 19;
+            this.txtTimeOut.Text = "5000";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(481, 20);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(64, 13);
+            this.label16.TabIndex = 13;
+            this.label16.Text = "Timeout(ms)";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(215, 165);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(690, 302);
+            this.dgv.TabIndex = 20;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 545);
+            this.ClientSize = new System.Drawing.Size(985, 545);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.txtTimeOut);
+            this.Controls.Add(this.txtUpdate);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.txtStartAddr);
             this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.txtAddrIp);
             this.Controls.Add(this.txtPort);
             this.Controls.Add(this.cmbRegType);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
             this.Controls.Add(this.lbStatus);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -227,6 +333,7 @@
             this.Name = "MainForm";
             this.Text = "Modbus TCP Read";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -251,6 +358,16 @@
         private System.Windows.Forms.TextBox txtAddrIp;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtStartAddr;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtUpdate;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox txtTimeOut;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.DataGridView dgv;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -1,14 +1,10 @@
-﻿using System;
+﻿using EasyModbus;
+using ModbusRead.Models;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Threading;
-using EasyModbus;
 
 namespace ModbusRead
 {
@@ -27,11 +23,16 @@ namespace ModbusRead
             {
                 if (btnConnect.Text == "CONNECT")
                 {
-                    modbusClient.IPAddress = "192.168.100.127";
-                    modbusClient.Port = 502;
-                    modbusClient.ConnectionTimeout = 5000;
+                    string ip = txtAddrIp.Text.Trim();
+                    int port = int.Parse(txtPort.Text.Trim());
+                    double autoupdate = double.Parse(txtUpdate.Text);
+                    int timeout = int.Parse(txtTimeOut.Text);
+
+                    modbusClient.IPAddress = ip;
+                    modbusClient.Port = port;
+                    modbusClient.ConnectionTimeout = timeout;
                     modbusClient.Connect();
-                    timerPoll.Interval = TimeSpan.FromSeconds(1);
+                    timerPoll.Interval = TimeSpan.FromSeconds(autoupdate);
                     timerPoll.Tick += timerPoll_Tick;
                     timerPoll.Start();
                     btnConnect.Text = "CONNECTED";
@@ -62,30 +63,158 @@ namespace ModbusRead
         {
             if (modbusClient.Connected == true)
             {
-                int[] vals = modbusClient.ReadHoldingRegisters(0, 10);
-                label1.Text = vals[0].ToString();
-                label2.Text = vals[1].ToString();
-                label3.Text = vals[2].ToString();
-                label4.Text = vals[3].ToString();
-                label5.Text = vals[4].ToString();
-                label6.Text = vals[5].ToString();
-                label7.Text = vals[6].ToString();
-                label8.Text = vals[7].ToString();
-                label9.Text = vals[8].ToString();
+                int startaddr = int.Parse(txtStartAddr.Text);
+                int qty = int.Parse(txtQty.Text);
 
-                textBox1.Text = null;
+                List<Readmodel> readmodel = new List<Readmodel>();
 
-                int[] hols = modbusClient.ReadInputRegisters(0, 10);
-                foreach (int i in hols) {
-                    textBox1.Text += i.ToString() + ",";
+                switch (cmbRegType.SelectedIndex)
+                {
+                    case 0:
+                        if (true)
+                        {
+                            bool[] vals = modbusClient.ReadCoils(startaddr, qty);
+
+                            for (int i = 0; i < qty; i++)
+                            {
+                                var model = new Readmodel()
+                                {
+                                    Id = i + 1,
+                                    Address = (0 + i).ToString().PadLeft(6, '0').Insert(3, " "),
+                                    HexValue = null,
+                                    DecValue = vals[i].ToString(),
+                                };
+                                readmodel.Add(model);
+
+                            }
+
+
+                        }
+
+                        break;
+                    case 1:
+                        if (true)
+                        {
+                            bool[] vals = modbusClient.ReadDiscreteInputs(startaddr, qty);
+                            for (int i = 0; i < qty; i++)
+                            {
+                                var model = new Readmodel()
+                                {
+                                    Id = i + 1,
+                                    Address = (200000 + i).ToString().PadLeft(6, '0').Insert(3, " "),
+                                    HexValue = null,
+                                    DecValue = vals[i].ToString(),
+                                };
+                                readmodel.Add(model);
+
+                            }
+
+                        }
+                        break;
+                    case 2:
+                        if (true)
+                        {
+                            int[] vals = modbusClient.ReadHoldingRegisters(startaddr, qty);
+                            for (int i = 0; i < qty; i++)
+                            {
+                                var model = new Readmodel()
+                                {
+                                    Id = i + 1,
+                                    Address = (300000 + i).ToString().PadLeft(6, '0').Insert(3, " "),
+                                    HexValue = null,
+                                    DecValue = vals[i].ToString(),
+                                };
+                                readmodel.Add(model);
+
+                            }
+
+                        }
+
+                        break;
+                    case 3:
+                        if (true)
+                        {
+                            int[] vals = modbusClient.ReadInputRegisters(startaddr, qty);
+                            for (int i = 0; i < qty; i++)
+                            {
+                                var model = new Readmodel()
+                                {
+                                    Id = i + 1,
+                                    Address = (400000 + i).ToString().PadLeft(6, '0').Insert(3, " "),
+                                    DecValue = vals[i].ToString(),
+                                };
+                                readmodel.Add(model);
+
+                            }
+
+
+                        }
+                        break;
                 }
-                
+
+                dgv.Rows.Clear();
+                var r = 0;
+                foreach (var item in readmodel)
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    row.CreateCells(dgv);
+                    row.Cells[0].Value = item.Id;
+                    row.Cells[1].Value = item.Address;
+                    row.Cells[2].Value = item.DecValue;
+                    if (item.DecValue == "True" || item.DecValue == "False")
+                    { 
+                    
+                    }
+                    else
+                    {
+
+
+                     var hexStr    = int.Parse(item.DecValue, System.Globalization.NumberStyles.HexNumber).ToString();
+                        row.Cells[3].Value = hexStr.PadLeft(4,'0');
+
+                    }
+                    //string.Format("{0:x}", item.DecValue);
+                    dgv.Rows.Add(row);
+
+                    r++;
+                }
+
+
+
             }
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
             cmbRegType.SelectedIndex = 0;
+            InitDvg();
+        }
+
+        private void InitDvg()
+        {
+            this.dgv.ColumnCount = 4;
+            this.dgv.Columns[0].Name = "No";
+            this.dgv.Columns[0].Width = 30;
+            this.dgv.Columns[0].SortMode = DataGridViewColumnSortMode.NotSortable;
+            this.dgv.Columns[1].Name = "Address";
+            this.dgv.Columns[1].Width = 200;
+            this.dgv.Columns[1].SortMode = DataGridViewColumnSortMode.NotSortable;
+            this.dgv.Columns[2].Name = "DEC";
+            this.dgv.Columns[2].Width = 200;
+            this.dgv.Columns[2].SortMode = DataGridViewColumnSortMode.NotSortable;
+            this.dgv.Columns[3].Name = "HEX";
+            this.dgv.Columns[3].Width = 200;
+            this.dgv.Columns[3].SortMode = DataGridViewColumnSortMode.NotSortable;
+            this.dgv.RowHeadersWidth = 30;
+            this.dgv.DefaultCellStyle.Font = new Font("Tahoma", 10);
+            this.dgv.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", 10);
+            this.dgv.RowHeadersWidth = 4;
+            this.dgv.RowTemplate.Height = 30;
+            this.dgv.RowsDefaultCellStyle.BackColor = Color.White;
+            this.dgv.AlternatingRowsDefaultCellStyle.BackColor = Color.PowderBlue;
+            dgv.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            dgv.AllowUserToResizeRows = false;
+            dgv.AllowUserToResizeColumns = false;
         }
     }
 }
