dec58822ebd4758fc041e23bacab8dfbd6563c5a
diatanuchit <anuchit.aree.a9f@ap.denso.com>
2024-09-16 14:26:33 +0700

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace ModbusRead
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cmbRegType = new System.Windows.Forms.ComboBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtAddrIp = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(185, 81);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(38, 13);
+            this.lbStatus.TabIndex = 13;
+            this.lbStatus.Text = "status:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(112, 347);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "label1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(112, 321);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "label1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(112, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(112, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(112, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(112, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(188, 39);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(97, 39);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "CONNECT";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 408);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(485, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // cmbRegType
+            // 
+            this.cmbRegType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRegType.FormattingEnabled = true;
+            this.cmbRegType.Items.AddRange(new object[] {
+            "01 (0x01) Read Coils",
+            "02 (0x02) Read Discrete Inputs",
+            "03 (0x03) Read Holding Registers",
+            "04 (0x04) Read Input Registers"});
+            this.cmbRegType.Location = new System.Drawing.Point(366, 39);
+            this.cmbRegType.Name = "cmbRegType";
+            this.cmbRegType.Size = new System.Drawing.Size(219, 21);
+            this.cmbRegType.TabIndex = 15;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(415, 177);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(77, 20);
+            this.txtPort.TabIndex = 16;
+            this.txtPort.Text = "502";
+            // 
+            // txtAddrIp
+            // 
+            this.txtAddrIp.Location = new System.Drawing.Point(254, 177);
+            this.txtAddrIp.Name = "txtAddrIp";
+            this.txtAddrIp.Size = new System.Drawing.Size(155, 20);
+            this.txtAddrIp.TabIndex = 17;
+            this.txtAddrIp.Text = "192.168.100.127";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(254, 158);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "IP Address";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(425, 161);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(26, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Port";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 545);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtAddrIp);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.cmbRegType);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "MainForm";
+            this.Text = "Modbus TCP Read";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cmbRegType;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtAddrIp;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using EasyModbus;
+
+namespace ModbusRead
+{
+    public partial class MainForm : Form
+    {
+        ModbusClient modbusClient = new ModbusClient();
+        DispatcherTimer timerPoll = new DispatcherTimer();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (btnConnect.Text == "CONNECT")
+                {
+                    modbusClient.IPAddress = "192.168.100.127";
+                    modbusClient.Port = 502;
+                    modbusClient.ConnectionTimeout = 5000;
+                    modbusClient.Connect();
+                    timerPoll.Interval = TimeSpan.FromSeconds(1);
+                    timerPoll.Tick += timerPoll_Tick;
+                    timerPoll.Start();
+                    btnConnect.Text = "CONNECTED";
+                    lbStatus.Text = "";
+                }
+                else
+                {
+                    modbusClient.Disconnect();
+                    btnConnect.Text = "CONNECT";
+                    timerPoll.Tick -= timerPoll_Tick;
+                }
+            }
+            catch (EasyModbus.Exceptions.ConnectionException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (System.Net.Sockets.SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+                lbStatus.Text = "Status : Connection timed out";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        private void timerPoll_Tick(object sender, EventArgs e)
+        {
+            if (modbusClient.Connected == true)
+            {
+                int[] vals = modbusClient.ReadHoldingRegisters(0, 10);
+                label1.Text = vals[0].ToString();
+                label2.Text = vals[1].ToString();
+                label3.Text = vals[2].ToString();
+                label4.Text = vals[3].ToString();
+                label5.Text = vals[4].ToString();
+                label6.Text = vals[5].ToString();
+                label7.Text = vals[6].ToString();
+                label8.Text = vals[7].ToString();
+                label9.Text = vals[8].ToString();
+
+                textBox1.Text = null;
+
+                int[] hols = modbusClient.ReadInputRegisters(0, 10);
+                foreach (int i in hols) {
+                    textBox1.Text += i.ToString() + ",";
+                }
+                
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            cmbRegType.SelectedIndex = 0;
+        }
+    }
+}
